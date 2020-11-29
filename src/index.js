882195b94e21d017ec5f751dcca19cbc68ad7a73
Donald Barthelemy <dond2979@gmail.com>
2020-11-29 13:50:10 -0600

    webpack not working

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
+import store from './store';
 import shoppingList from './shopping-list';
 
-const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+const main = function(){
+  api.getItems()
+   .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+});
+   
+   shoppingList.bindEventListeners();
+   shoppingList.render();
 };
 
 $(main);
